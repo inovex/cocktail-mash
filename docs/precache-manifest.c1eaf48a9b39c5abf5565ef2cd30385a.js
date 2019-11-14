93e39b09f308865836624bbae379367049602f4c
Robert Seibert <robert.seibert@inovex.de>
2019-11-14 14:56:47 +0100

    add static files

---
+++ docs/precache-manifest.c1eaf48a9b39c5abf5565ef2cd30385a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5685d1f64437332c6a6fd138f36ada7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9111aea621d904e04ef4",
+    "url": "/static/css/main.58eba2fb.chunk.css"
+  },
+  {
+    "revision": "9e0847d0c17008e051ed",
+    "url": "/static/js/2.9d60e397.chunk.js"
+  },
+  {
+    "revision": "9111aea621d904e04ef4",
+    "url": "/static/js/main.3247d596.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "018c747c3dceca21441305889e2249c0",
+    "url": "/static/media/LeckerliOne-Regular.018c747c.ttf"
+  }
+]);
