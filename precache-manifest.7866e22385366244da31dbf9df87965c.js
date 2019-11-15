b43811a00aa3a26e42f53ab81c03fb3fb165aa27
Robert Seibert <robert.seibert@inovex.de>
2019-11-15 13:15:42 +0100

    Updates

---
+++ precache-manifest.7866e22385366244da31dbf9df87965c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6380f25d018521b97069f869969384b",
+    "url": "/cocktail-mash/index.html"
+  },
+  {
+    "revision": "f3c5c4e128daa3d459a6",
+    "url": "/cocktail-mash/static/css/main.2703d6ac.chunk.css"
+  },
+  {
+    "revision": "2cd865eb7b086ba09b05",
+    "url": "/cocktail-mash/static/js/2.532e1df4.chunk.js"
+  },
+  {
+    "revision": "f3c5c4e128daa3d459a6",
+    "url": "/cocktail-mash/static/js/main.af39541f.chunk.js"
+  },
+  {
+    "revision": "58b7a220e048bd942a74",
+    "url": "/cocktail-mash/static/js/runtime~main.b1f29d77.js"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/cocktail-mash/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "018c747c3dceca21441305889e2249c0",
+    "url": "/cocktail-mash/static/media/LeckerliOne-Regular.018c747c.ttf"
+  }
+]);
