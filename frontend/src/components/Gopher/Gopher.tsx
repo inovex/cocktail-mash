0e65aecf0734c646b6a5d78abaa9cddddcec7ebf
Robert Seibert <robert.seibert@inovex.de>
2019-11-15 13:25:53 +0100

    remove docs folder, add github deploy script

--- frontend/src/components/Gopher/Gopher.tsx
+++ frontend/src/components/Gopher/Gopher.tsx
@@ -8,10 +8,10 @@ const Gopher = (props: { reach?: boolean, speechBubble?: any } = { reach: true }
         {props.speechBubble}
         <span className="Gopher__Wrapper">
             <span className={cx("Gopher", { "Gopher--reaching": props.reach })}>
-                <img alt="" className="Gopher__Frame" src="/images/Gopher__Frame.png"/>
-                <img alt="" className="Gopher__LeftArm" src="/images/Gopher__LeftArm.png"/>
-                <img alt="" className="Gopher__RightArm" src="/images/Gopher__RightArm.png"/>
-                <img alt="" className="Gopher__Eyes--closed" src="/images/Gopher__Eyes--closed.png"/>
+                <img alt="" className="Gopher__Frame" src="images/Gopher__Frame.png"/>
+                <img alt="" className="Gopher__LeftArm" src="images/Gopher__LeftArm.png"/>
+                <img alt="" className="Gopher__RightArm" src="images/Gopher__RightArm.png"/>
+                <img alt="" className="Gopher__Eyes--closed" src="images/Gopher__Eyes--closed.png"/>
             </span>
         </span>
     </div>;
